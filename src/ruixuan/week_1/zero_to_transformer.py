753997a2719b68850b6b1d372bbd4be63f98303b
Rick0037 <19s053048@stu.hit.edu.cn>
2026-08-17 18:35:45 +0800

    finish transform

--- src/ruixuan/week_1/zero_to_transformer.py
+++ src/ruixuan/week_1/zero_to_transformer.py
@@ -1,4 +1,5 @@
 import torch
+from torch._dynamo.polyfills.pytree import none_unflatten
 import torch.nn as nn
 import math
 
@@ -87,6 +88,8 @@ class FeedForward(nn.Module):
         return self.net(x)
 
 
+# 为例防止layer norm 后面直接给一个relu 过滤一半的梯度
+# 实际操作我们使用 gamma 以及 beta 来防止这种情况发生
 class LayerNorm(nn.Module):
     def __init__(self, d_model, eps=1e-12):
         super().__init__()
@@ -133,6 +136,11 @@ print(ln.beta.data)  # [0.1000, 0.1000, 0.1000, 0.1000]  ← 从 0.0 变了
 
 # PE(pose, 2i) = sin(pose / 10000 2i/dmoel)
 # PE(pose, 2i+1) = cos(pose/ 10000 2i+1)
+# 1. 先写初始化pe
+# 2. 后关注pos
+# 3. 再关注div 是什么样子的
+# 4. 最后写sin 以及cos的运算，
+# 最终的结果应该是max length 和 dmodel 广播出来的结果
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
         super().__init__()
@@ -152,6 +160,7 @@ class PositionalEncoding(nn.Module):
         # 偶数维用sin，奇数维用cos
         # pe[:, 0::2] 针对每一行 以0为间隔
         # pe[:, 0::2] 针对每一行 以1为间隔
+        # TODO： 对所有行，取第 0 列开始、步长为 2 的列 （即偶数索引列）。
         pe[:, 0::2] = torch.sin(pos * div)  # [max_len, D/2]
         pe[:, 1::2] = torch.cos(pos * div)  # [max_len, D/2]
 
@@ -203,3 +212,106 @@ class Mask_Address:
         return pad_mask & causal_mask  # [B, 1, L, L]
 
     # 广播运算 与padding 还有 上三角掩码同时计算出的结果
+
+
+# PE 不算 encoder 或者decoder block中的内容
+class EncoderBlock(nn.Module):
+    def __init__(self, d_model, n_head, d_ff) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.n_head = n_head
+        self.d_ff = d_ff
+        self.attn = MultiHeadAttention(d_model, n_head)
+        self.ffn = FeedForward(d_model, d_ff)
+        # 物理上有几个就需要有几个
+        self.layer_norm_1 = LayerNorm(d_model)
+        self.layer_norm_2 = LayerNorm(d_model)
+
+    # 在多个batch
+    def forward(self, x, src_mask):
+        # [输入 B,L,D]
+        step_one = self.layer_norm_1(x + self.attn(x, x, x, src_mask))
+        # [输出 B,L,D] 中间的过程被忽略掉了
+        return self.layer_norm_1(step_one, x.ffn(step_one))
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, d_model, n_head, d_ff) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.n_head = n_head
+        self.d_ff = d_ff
+        # TODO: MultiHeadAttention 中是有权重的，输入只需要类似x矩阵
+        self.attn_1 = MultiHeadAttention(d_model, n_head)
+        self.attn_2 = MultiHeadAttention(d_model, n_head)
+        self.ffn = FeedForward(d_model, d_ff)
+        # 物理上有几个就需要有几个
+        self.layer_norm_1 = LayerNorm(d_model)
+        self.layer_norm_2 = LayerNorm(d_model)
+        self.layer_norm_3 = LayerNorm(d_model)
+
+    def forward(self, x, enc_out, src_mask, tgt_mask):
+        # 这里是上三角mask的
+        step_one = self.layer_norm_1(x + self.attn_1(x, x, x, tgt_mask))
+
+        # 这里是src mask
+        step_two = self.layer_norm_2(
+            step_one + self.attn_2(step_one, enc_out, enc_out, src_mask)
+        )
+
+        step_three = self.layer_norm_3(step_two, self.ffn(step_two))
+
+        return step_three
+
+
+class Transformer(nn.Module):
+
+    def __init__(self, vocab_size, d_model, n_heads, d_ffn, n_layers):
+        """_summary_
+        Args:
+            vocab_size (_type_): nn embedding vocab size, how mamy vocab support
+            d_model (_type_): _description_
+            n_heads (_type_): _description_
+            d_ffn (_type_): _description_
+            n_layers (_type_): _description_
+        """
+        super().__init__()
+        self.emb = nn.Embedding(vocab_size, d_model)
+        self.pos = PositionalEncoding(d_model)
+        self.mask_method = Mask_Address()
+
+        # 整个两个MHA 就是一个encoder block
+        self.encoder = nn.ModuleList(
+            [EncoderBlock(d_model, n_heads, d_ffn) for _ in range(n_layers)]
+        )
+        # 整个三个MHA 就是一个decoder block
+        self.decoder = nn.ModuleList(
+            [DecoderBlock(d_model, n_heads, d_ffn) for _ in range(n_layers)]
+        )
+        # 输出投影到词表: [B, L, D] → [B, L, V]
+        self.fc_out = nn.Linear(d_model, vocab_size)
+
+    # forward 同时支持训练和推理:
+    # - 训练: tgt 是完整序列 [<bos>, 我, 爱, 学], 一次 forward 算所有 loss (teacher forcing)
+    # - 推理: tgt 从 [<bos>] 开始, 每步生成一个词拼到末尾, 循环直到 <eos>
+    # PyTorch 的 nn.Module 自带 self.training 属性：
+    # - model.train() → self.training = True
+    # - model.eval() → self.training = False
+    def forward(self, src, tgt):
+        src_mask = self.mask_method.make_src_mask(src)
+        tgt_mask = self.mask_method.make_tgt_mask(tgt)
+
+        enc = self.emb(src)
+        enc = self.pos(enc)
+        for layer in self.encoder:
+            # 对应的是encoder的参数输入
+            enc = layer(enc, src_mask)
+
+        dec = self.pos(self.emb(tgt))
+
+        for layer in self.decoder:
+            # 对应的是decoder的参数输入
+            dec = layer(dec, enc, src_mask, tgt_mask)
+
+        return self.fc_out(dec)  #  [B, L_tgt, D] -> [B, L_tgt, V]
+        # Softmax → 词表概率分布
